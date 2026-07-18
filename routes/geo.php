073520e1d518a44252441288714d71b1d7cd4a5e
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2026-07-18 13:20:55 +1000

    Use snake_case route name (geo.llms_txt) per style guide

--- routes/geo.php
+++ routes/geo.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use JeanPierreGassin\LaravelGeo\Http\Controllers\LlmsTxtController;
 
 Route::get(config('geo.llms_txt.path'), LlmsTxtController::class)
-    ->name('geo.llms-txt');
+    ->name('geo.llms_txt');
